dcb29e2aeddc29b9fe36bfdd9eebdc09ee7c5742
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-10 12:00:12 -0500

    made display menu

--- src/ContactsManager/ContactsManagerProject.java
+++ src/ContactsManager/ContactsManagerProject.java
@@ -6,8 +6,27 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class ContactsManagerProject {
+
+	public static Input input = new Input();
+
+	public static int displayMenu() {
+		System.out.println("1. View contacts");
+		System.out.println("2. Add a new contact");
+		System.out.println("3. Search a contact by name");
+		System.out.println("4. Delete an existing contact");
+		System.out.println("5. Exit");
+		System.out.println("Enter an option (1, 2, 3, 4 or 5): ");
+		int userMenuResponse = input.getInt(1, 5);
+		return userMenuResponse;
+
+
+	}
+
+
 	public static void createFile() {
 
 		String directory = "data";
@@ -63,7 +82,7 @@ public class ContactsManagerProject {
 	public static void main (String[]args){
 		createFile();
 		initialize();
-
+		displayMenu();
 
 	}
 
